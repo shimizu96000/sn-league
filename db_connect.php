49a183b595e732cadb330fea4bca55b123ea9ef0
h-shimizu <hina.shi.2006@icloud.com>
2026-01-09 13:46:52 +0900

    バグ修正6

--- db_connect.php
+++ db_connect.php
@@ -1,11 +1,23 @@
 <?php
 // データベース接続設定
 $dbname = 'mahjong_db';
-$username = 'sn_league';
-$password = 'sn_league_pass_123';
+
+// 環境判定：ラズパイか XAMPP か
+$is_raspi = file_exists('/etc/os-release');
+$is_xampp = strpos(__FILE__, 'xampp') !== false || getenv('XAMPP_HOME');
+
+if ($is_raspi && !$is_xampp) {
+    // ラズパイ本番環境
+    $username = 'sn_league';
+    $password = 'sn_league_pass_123';
+} else {
+    // XAMPP ローカル環境
+    $username = 'root';
+    $password = '';
+}
 
 try {
-    // TCP で接続（ラズパイとXAMPP両方対応）
+    // TCP で接続
     $pdo = new PDO(
         "mysql:host=127.0.0.1;port=3306;dbname=$dbname;charset=utf8mb4",
         $username,
